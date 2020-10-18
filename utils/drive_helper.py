eaba688898924b03e886971a008850529ce8e2f5
Loh Zheng Yi <nuke504@gmail.com>
2020-10-18 14:34:06 +0800

    Fixed drive bug

--- utils/drive_helper.py
+++ utils/drive_helper.py
@@ -10,9 +10,9 @@ def extract_data_g_drive(g_drive_path):
         str g_drive_path: preferably os.path, else can also be string showing path to zip file in google drive. zip file should be named data.zip
     '''
     cwd = os.getcwd()
-    drive.mount(cwd)
+    drive.mount(os.path.join(cwd,'drive'))
 
-    data_path = os.path.join(cwd,'My Drive',g_drive_path)
+    data_path = os.path.join(os.path.join(cwd,'drive'),'My Drive',g_drive_path)
     if os.path.exists(data_path):
         shutil.copy(os.path.join(data_path), os.path.join(cwd,'data.zip'))
         with zipfile.ZipFile(os.path.join(cwd,'data.zip'),"r") as zip_ref:
