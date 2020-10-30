379033bb19f5b46551d5b1d0bcd1730acb27482a
Zheng Yi <42016207+nuke504@users.noreply.github.com>
2020-10-30 17:25:42 +0800

    FIXED CRITICAL BUG
    Changed mean axis from 1 to 3

--- utils/custom_layers.py
+++ utils/custom_layers.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 '''
 Code below adapted from: https://github.com/facebookresearch/pytorch_GAN_zoo/ re-written for tensorflow 2.x
 '''
-def Upscale2d(X, factor=2):
-    assert isinstance(factor, int) and factor >= 1
-    if factor == 1:
-        return X
-    s = X.shape
-    X = tf.reshape(X,(s[0], s[1], 1, s[2], 1, s[3]))
-    X = tf.broadcast_to(X, (s[0], s[1], factor, s[2], factor, s[3]))
-    X = tf.reshape(X,(s[0], s[1] * factor, s[2] * factor, s[3]))
-    return X
+# def Upscale2d(X, factor=2):
+#     assert isinstance(factor, int) and factor >= 1
+#     if factor == 1:
+#         return X
+#     s = X.shape
+#     X = tf.reshape(X,(s[0], s[1], 1, s[2], 1, s[3]))
+#     X = tf.broadcast_to(X, (s[0], s[1], factor, s[2], factor, s[3]))
+#     X = tf.reshape(X,(s[0], s[1] * factor, s[2] * factor, s[3]))
+#     return X
 
 def get_layer_normalisation_factor(weight_shape):
     """
@@ -19,35 +19,44 @@ def get_layer_normalisation_factor(weight_shape):
     """
     return tf.math.sqrt(tf.constant(2.0)/tf.math.reduce_prod(tf.cast(weight_shape, tf.float32)[1:]))
 
-def mini_batch_sd(X, subGroupSize=4, epsilon=1e-8):
-    """
-    Add a minibatch standard deviation channel to the current layer.
-    In other words:
-        1) Compute the standard deviation of the feature map over the minibatch
-        2) Get the mean, over all pixels and all channels of thsi ValueError
-        3) expand the layer and cocatenate it with the input
-    Args:
-        - x (tensor): previous layer
-        - subGroupSize (int): size of the mini-batches on which the standard deviation
-        should be computed
-    """
-    shape = X.shape
-    subGroupSize = min(shape[0], subGroupSize)
-    if shape[0] % subGroupSize != 0:
-        subGroupSize = shape[0]
-    G = int(shape[0] / subGroupSize)
-    if subGroupSize > 1:
-        y = tf.reshape(X, (-1, subGroupSize, shape[1], shape[2], shape[3]))
-        y = tf.math.sqrt(tf.math.reduce_variance(y, axis = 3) + epsilon)
-        y = tf.reshape(y, (G, -1))
-        y = tf.reshape(tf.math.reduce_mean(y, axis=1), (G, 1))
-        y = tf.reshape(tf.broadcast_to(y, (G, shape[1]*shape[2])), (G, 1, shape[1], shape[2], 1))
-        y = tf.broadcast_to(y, (G, subGroupSize, shape[1], shape[2], 1))
-        y = tf.reshape(y, (-1, shape[1], shape[2], 1))
-    else:
-        y = tf.zeros((shape[0], shape[1], shape[2], 1))
+# def mini_batch_sd(X, subGroupSize=4, epsilon=1e-8):
+#     """
+#     Add a minibatch standard deviation channel to the current layer.
+#     In other words:
+#         1) Compute the standard deviation of the feature map over the minibatch
+#         2) Get the mean, over all pixels and all channels of thsi ValueError
+#         3) expand the layer and cocatenate it with the input
+#     Args:
+#         - x (tensor): previous layer
+#         - subGroupSize (int): size of the mini-batches on which the standard deviation
+#         should be computed
+#     """
+#     shape = X.shape
+#     subGroupSize = min(shape[0], subGroupSize)
+#     if shape[0] % subGroupSize != 0:
+#         subGroupSize = shape[0]
+#     G = int(shape[0] / subGroupSize)
+#     if subGroupSize > 1:
+#         y = tf.reshape(X, (-1, subGroupSize, shape[1], shape[2], shape[3]))
+#         y = tf.math.sqrt(tf.math.reduce_variance(y, axis = 3) + epsilon)
+#         y = tf.reshape(y, (G, -1))
+#         y = tf.reshape(tf.math.reduce_mean(y, axis=1), (G, 1))
+#         y = tf.reshape(tf.broadcast_to(y, (G, shape[1]*shape[2])), (G, 1, shape[1], shape[2], 1))
+#         y = tf.broadcast_to(y, (G, subGroupSize, shape[1], shape[2], 1))
+#         y = tf.reshape(y, (-1, shape[1], shape[2], 1))
+#     else:
+#         y = tf.zeros((shape[0], shape[1], shape[2], 1))
 
-    return tf.concat([X, y], axis=3)
+#     return tf.concat([X, y], axis=3)
+
+def mini_batch_sd(X):
+    batch_size, h, w, _ = X.get_shape().as_list()
+    new_feat_shape = [batch_size, h, w, 1]
+
+    mean, var = tf.nn.moments(X, axes=[0], keep_dims=True)
+    stddev = tf.math.sqrt(tf.reduce_mean(var, keep_dims=True))
+    new_feat = tf.tile(stddev, multiples=new_feat_shape)
+    return tf.concat([X, new_feat], axis=3)
 
 class NormalizationLayer(tf.keras.layers.Layer):
     '''
@@ -57,7 +66,7 @@ class NormalizationLayer(tf.keras.layers.Layer):
         super(NormalizationLayer, self).__init__(**kwargs)
     
     def call(self, X, epsilon=1e-8):
-        return X * tf.math.rsqrt(tf.math.reduce_mean(X**2, axis=1, keepdims=True) + epsilon)
+        return X * tf.math.rsqrt(tf.math.reduce_mean(X**2, axis=3, keepdims=True) + epsilon)
 
 class EqualizerInitializer(tf.keras.initializers.Initializer):
 
