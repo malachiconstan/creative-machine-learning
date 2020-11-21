449b11f1b027e0e96d16ffdb434cbc0af5906f1e
Loh Zheng Yi <nuke504@gmail.com>
2020-11-21 21:49:58 +0800

    fixed bug

--- utils/train.py
+++ utils/train.py
@@ -704,8 +704,9 @@ class ProgressiveGANTrainer(object):
         if value < 0 or value > 1:
             raise ValueError("alpha must be in [0,1]")
 
-        self.model.alpha = value
-        self._alpha = value
+        if value != self._alpha:
+            self.model.alpha = value
+            self._alpha = value
 
     def train(self, restore=False, colab=False, load_from_g_drive=False, verbose=False, g_drive_path = '/content/drive/My Drive/CML'):
         """
