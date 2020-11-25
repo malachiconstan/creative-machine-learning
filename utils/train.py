69d3d399b0c6229b1a3182af37109ed7438f0c8e
ZY <nuke504@gmail.com>
2020-11-26 07:43:42 +0800

    updated utils

--- utils/train.py
+++ utils/train.py
@@ -847,7 +847,7 @@ class ProgressiveGANTrainer(object):
             # Fade in half of switch_res_every_n_epoch epoch, and stablize another half
             self.resolution_alpha_increment = 1. / (self.epochs / 2 * training_steps)
             if self.hard_start:
-                self.alpha = 1
+                self.alpha = 0.9
             else:
                 self.alpha = min(1., (self.start_epoch - 1) % self.epochs * training_steps * self.resolution_alpha_increment)
             assert self.start_epoch <= self.epochs, f'Start epochs {self.start_epoch} should be less than epochs: {self.epochs}'
